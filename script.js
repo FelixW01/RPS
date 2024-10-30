5cf62a1263b98832bd56a59255380a0ca87472d2
Felix W <felixwillem01@yahoo.com>
2024-10-29 23:33:46 -0400

    added game reset on start functionality

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const aiArr = ['rock', 'paper', 'scissors']
 const aiCardArr = ["./public/images/rock.png", "./public/images/paper.png", "./public/images/scissors.png"]
 const aiBack = './public/images/card.png'; 
 let rounds = 1
+const aiCards = document.querySelectorAll('.ai-card');
 
-
+// Initializes rounds, evoked on page load
 function initializeGame() {
 contentEl.innerHTML = `<p></p>
         <h1>Game ${rounds}</h1>
@@ -13,14 +14,25 @@ contentEl.innerHTML = `<p></p>
         <p></p>`
 }
 
-
+// Resets cards, return the image to the back of the card, remove class to re-trigger animation when re-added
+function resetCards() {
+    aiCards.forEach(card => {
+        card.src = aiBack;
+        card.classList.remove('animate__flipInY', 'animate__flipOutY');
+    });
+}
 
 // AI Logic, RNG 0-2 and checks it against ai array to figure out which move the AI makes
 function aiMove() {
     const rng = Math.floor(Math.random() * 3)
     const aiCard = document.getElementById(`${rng + 1}`)
-    aiCard.src = aiCardArr[rng]
-    aiCard.classList.add('animate__flipInY')
+    // Resets cards before a new round starts
+    resetCards();
+    // Tiny grace period before the animation starts to give resetCards some time
+    setTimeout(() => {
+        aiCard.src = aiCardArr[rng]
+        aiCard.classList.add('animate__flipInY')
+    }, 200);
     return aiArr[rng]
 }
 
@@ -41,7 +53,6 @@ selectionBtns.forEach(selectionBtn => {
 
 // Checks with conditionals all the 9 possibilities that can happen
 function makeSelection(selection) {
-    console.log(selection, '<<<<<<<<<<<< USER SELECTION')
     const aiSelection = aiMove();
 
     let outcomeMessage = 'You Lose!'
@@ -50,14 +61,11 @@ function makeSelection(selection) {
     // If user selection is equals to AI selection, it's a tie
     if (selection === aiSelection) {
         outcomeMessage = "It's a tie!"
-        console.log("It's a tie!")
     // Else if the outcome[selection], this code matches the user selection to the key of outcome and if the value of said key is equal to aiSelection then you've won
     } else if (outcomes[selection] === aiSelection) {
         outcomeMessage = "You Won!"
-        console.log('You won!')
     // Every other scenario ends in a loss
     } else {
-        console.log('You Lost!')
     }
     const template = `
         <p>${aiSelection.toUpperCase()}</p>
